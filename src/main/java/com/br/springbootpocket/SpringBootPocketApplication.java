a0bc5f78b78ef019aea701b7b19a89943ebe63ee
thiagohernandes <hernandes_sp@yahoo.com.br>
2019-02-04 15:14:47 -0200

    Criacao do projeto

---
+++ src/main/java/com/br/springbootpocket/SpringBootPocketApplication.java
@@ -0,0 +1,14 @@
+package com.br.springbootpocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPocketApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/br/springbootpocket/SpringBootPocketApplicationTests.java
@@ -0,0 +1,17 @@
+package com.br.springbootpocket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootPocketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
