e3466f3cf7fef7299a507e1a2922e86d163c7fdb
thiagohernandes <hernandes_sp@yahoo.com.br>
2019-02-04 16:33:48 -0200

    REST Hello World

--- src/main/java/com/br/SpringBootPocketApplication.java
+++ src/main/java/com/br/SpringBootPocketApplication.java
@@ -1,4 +1,4 @@
-package com.br.springbootpocket;
+package com.br;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/br/controller/BasicController.java
@@ -0,0 +1,12 @@
+package com.br.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+  @GetMapping("/welcome")
+  public String welcome() {
+    return "Hello World";
+  }
+}
